810cdd6c3d1c87812c7d83c03e8d016e66141a97
hilerioluwa12 <hardexilerioluwa2gmail.com>
2022-12-15 14:19:13 -0800

    101-binary_tree_levelorder.c

--- binary_trees.h
+++ binary_trees.h
@@ -65,8 +65,5 @@ size_t binary_tree_height(const binary_tree_t *tree);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
-void recursion(link_t **head, const binary_tree_t *tree);
-void linked_node(link_t **head, const binary_tree_t *tree, size_t level);
-
 
 #endif
