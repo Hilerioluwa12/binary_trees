261d53ab2d7dbec295ef4d5f3784209cb49c86f3
hilerioluwa12 <hardexilerioluwa2gmail.com>
2022-12-15 14:13:54 -0800

    101-binary_tree_levelorder.c

--- binary_trees.h
+++ binary_trees.h
@@ -23,6 +23,21 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
+/* linked list for advanced 101 the levelorder */
+/**
+ * struct link_s - structure for advanced tasks
+ *
+ * @n: depth of node specified
+ * @node: node of tree to store
+ * @next: next node of the linked list
+ */
+typedef struct link_s
+{
+	size_t n;
+	struct binary_tree_s const *node;
+	struct link_s *next;
+} link_t;
+
 /* mandatory task */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -47,6 +62,11 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 size_t binary_tree_height(const binary_tree_t *tree);
 
 /* advanced task */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+const binary_tree_t *second);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+void recursion(link_t **head, const binary_tree_t *tree);
+void linked_node(link_t **head, const binary_tree_t *tree, size_t level);
+
 
 #endif
