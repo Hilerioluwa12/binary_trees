26d9a2965dd3a2b3f038f08cebb5f745daac7fa8
Motaz-Mukhtar <motazksa2@gmail.com>
2022-12-14 22:23:28 -0800

    hotfix

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 	binary_tree_t *temp;
 
-	if (parent == NULL || new_node == NULL)
+	if (parent == NULL)
 		return (NULL);
 
 	new_node = binary_tree_node(parent, value);
